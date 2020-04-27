d8293f5130166ea59d2f56f81f9263bc8b562d59
Shreyas Y. Bhise <syb29@kill.cs.rutgers.edu>
2020-04-26 23:04:24 -0400

    Server is running

--- ServerStuff/server.c
+++ ServerStuff/server.c
@@ -1,2 +1,56 @@
 #include<stdio.h>
 #include<stdlib.h>
+#include<unistd.h>
+#include<sys/socket.h>
+#include<sys/stat.h>
+#include<string.h>
+#include<netinet/in.h>
+void error(char* msg) {
+	printf("ERROR: %s\n", msg);
+	exit(1);
+}
+int main(int argc, char** argv) {
+	int sockfd = -1; //fd for socket
+	int newsockfd = -1; //fd for client socket
+	int portno = -1; //server port to connect to
+	int clilen = -1; //utility variable - size of clientAddressInfo below
+	int n = -1; //utility variable - monitoring reading/writing to/from socket
+	char buffer[256]; //char array to store data moving to and from socket
+	
+	struct sockaddr_in sAddrInfo; //holds address info for building server socket
+	struct sockaddr_in cAddrInfo; //holds address info for building client socket
+	
+	if(argc < 2) { error("no port provided."); }
+
+	portno = atoi(argv[1]); //Convert text representation of port number
+	
+	/***SOCKET SET UP***/
+	sockfd = socket(AF_INET, SOCK_STREAM, 0);
+	if(sockfd < 0) { error("could not open socket."); }
+
+	bzero((char *) &sAddrInfo, sizeof(sAddrInfo));
+	sAddrInfo.sin_port = htons(portno);
+	sAddrInfo.sin_family = AF_INET;
+	sAddrInfo.sin_addr.s_addr = INADDR_ANY;
+
+	/***SOCKET BINDING***/
+	if (bind(sockfd, (struct sockaddr *) &sAddrInfo, sizeof(sAddrInfo)) < 0) { error("Could not bind socket to port."); }
+
+	listen(sockfd, 0);
+
+	clilen = sizeof(cAddrInfo);
+	while(1) {
+	newsockfd = accept(sockfd, (struct sockaddr *) &cAddrInfo, &clilen);
+	
+	if(newsockfd < 0) { error("accept failed."); }
+	
+		bzero(buffer, 256);	
+		n = read(newsockfd, buffer, 255);
+		if(n < 0) {error("could not read from socket."); }
+	
+		printf("Here is the message: %s\n", buffer);
+		n = write(newsockfd, "yerr", 18);
+		
+	}
+	return 0;
+}
