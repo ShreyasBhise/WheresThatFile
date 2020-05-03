88ec5327225509c606b1dbc0b476e342a618f326
Shreyas Y. Bhise <syb29@kill.cs.rutgers.edu>
2020-05-03 19:06:20 -0400

    Refactored utility functions

---
+++ ServerStuff/sUtil.c
@@ -0,0 +1,110 @@
+#include<stdio.h>
+#include<stdlib.h>
+#include<unistd.h>
+#include<sys/socket.h>
+#include<sys/stat.h>
+#include<string.h>
+#include<netinet/in.h>
+#include<pthread.h>
+#include<sys/types.h>
+#include<sys/stat.h>
+#include<fcntl.h>
+#include"readManifest.h"
+#include"server.h"
+
+void error(char* msg) {
+	printf("ERROR: %s\n", msg);
+	exit(1);
+}
+int isChanged(char* fileName, node* root){
+	node* ptr;
+	for(ptr = root; ptr!=NULL; ptr=ptr->next){
+		if(strcmp(fileName, root->filePath)==0){
+			return 1;
+		}
+	}
+	return 0;
+}
+char* removeProjName(char* filePath){
+	char c;
+	int i = 0;
+	while(i<strlen(filePath)){
+		c = filePath[i];
+		if(c=='/'){
+			char* newName = (char*)malloc(256);
+			strcpy(newName, filePath+i);
+			return newName;
+		}
+		i++;
+	}
+	return NULL;
+}
+void writeCommit(int fd, node* root){
+	node* ptr;
+	for(ptr = root; ptr!=NULL; ptr = ptr->next){
+		char toWrite[256];
+		sprintf(toWrite, "M\t%d\t%s\t%s\n", ptr->version, ptr->filePath, ptr->hash);
+		int n = write(fd, toWrite, strlen(toWrite)); 
+	}
+	return;
+}
+projNode* searchPNode(char* pName) { //Return pointer to the projectNode matching the project name that is given.
+	projNode* ptr;
+	for(ptr = projRoot; ptr != NULL; ptr = ptr->next) {
+		printf("pNode: %s\n", ptr->projName);
+		if(strcmp(pName, ptr->projName) == 0) {
+			return ptr;
+		}
+	}
+	return NULL;
+}
+char* getSize(int fd) {
+	int fileSize = lseek(fd, 0, SEEK_END);
+	lseek(fd, 0, SEEK_SET);
+	char* sizeStr = malloc(10 * sizeof(char)) ;
+	sprintf(sizeStr, "%d", fileSize);
+	return sizeStr;
+}
+void sendFile(int sockfd, int fd) { // sends the size of the file, a space, then the contents of the file
+	int n;
+	char* sizeStr = getSize(fd);
+	int size = atoi(sizeStr);
+	write(sockfd, strcat(sizeStr, "\t"), strlen(sizeStr) + 1);
+	printf("Sent back to client [%s ]\n", sizeStr);
+	char* buffer = malloc(size * sizeof(char) + 1);
+	n = read(fd, buffer, size);
+	if (n <= 0) { error("could not read file.\n"); }
+
+	
+	n = write(sockfd, buffer, size);
+	printf("Sent back to client %d bytes [%s ]\n",size, buffer);
+
+}
+int projectExists(char* projectName) {
+	struct stat st;
+	if (stat(projectName, &st) != -1) { 
+		return 1;
+	}
+	return 0;	
+}
+char* getProjName(int sockfd) {
+	char* buffer = (char *) malloc(256);
+	int n = 0;
+	while((n = read(sockfd, buffer, 255)) == 0)
+	if(n < 0) { error("Could not read from socket."); }
+	buffer[n] = '\0';
+	
+	return buffer;
+}
+char* extractFileNameFromPath(char* path) {
+	char* fileName = strrchr(path, '/');
+	return (fileName + 1);
+}
+int getVersion(char* projName){
+	char manifestPath[256];
+	sprintf(manifestPath, "%s/.Manifest", projName);	
+	int manfd = open(manifestPath, O_RDONLY);
+	int x = readNum(manfd);
+	close(manfd);
+	return x;
+}
---
+++ ServerStuff/server.h
@@ -0,0 +1,38 @@
+#ifndef serverFunc
+#define serverFunc
+typedef struct commitNode{
+	char* file;
+	int size;
+	struct commitNode* next;
+} commitNode;
+
+typedef struct projNode {
+	char* projName;
+	struct commitNode* commitListRoot;
+	struct projNode* next;
+} projNode;
+
+projNode* projRoot;
+
+void error (char *msg);
+int isChanged (char *fileName, node *root);
+char *removeProjName (char *filePath);
+void writeCommit (int fd, node *root);
+projNode *searchPNode (char *pName);
+char *getSize (int fd);
+void sendFile (int sockfd, int fd);
+int projectExists (char *projectName);
+
+char *getProjName (int sockfd);
+int destroyProject (int sockfd);
+int createProject (int sockfd);
+char *extractFileNameFromPath (char *path);
+int currentVersion (int sockfd);
+int getVersion (char *projName);
+int checkout (int sockfd);
+int commit (int sockfd);
+int push (int sockfd);
+
+void *clientConnect (void *clientSockfd);
+int main (int argc, char **argv);
+#endif //serverFunc
