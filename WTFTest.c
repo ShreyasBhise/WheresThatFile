c50045e1562a4d5b7b2670bff888b35d2da98d32
Shreyas <29694206+ShreyasBhise@users.noreply.github.com>
2020-05-05 20:10:16 -0400

    Update WTFTest.c

--- WTFTest.c
+++ WTFTest.c
@@ -1,24 +1,32 @@
 #include <sys/types.h>
 #include <sys/stat.h>
 #include <fcntl.h>
+#include <stdio.h>
+#include <stdlib.h>
 //Run TEST code
 int main(int argc, char** argv){
-	int fd = open(".Commands", RD_ONLY);
-	char* cmd = (char*)malloc(256);
-	char c;
-	int n;
-	int i = 0;
-	while((n = read(fd, &c, 1))>0){
-		if(c=='\n'){
-			cmd[i]='\0';
-			system(cmd);
-			i = 0;
-			bzero(cmd, 255);
-		} else {
-			cmd[i] = c;
-			i++;
+	int pid = fork();
+	
+	if(pid == 0) {//Child Process -> Should be server.
+		system("Server/WTFServer localhost 6782&");
+	} else { //Parent Process -> Should handle client.
+		int fd = open(".Commands", RD_ONLY);
+		char* cmd = (char*)malloc(256);
+		char c;
+		int n;
+		int i = 0;
+		while((n = read(fd, &c, 1))>0){
+			if(c=='\n'){
+				cmd[i]='\0';
+				system(cmd);
+				i = 0;
+				bzero(cmd, 255);
+			} else {
+				cmd[i] = c;
+				i++;
+			}
 		}
+		free(cmd);
 	}
-	free(cmd);
 	return 0;
 }
