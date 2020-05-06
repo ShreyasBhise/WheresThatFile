47e08fcda5166a7f75699dd034848a47f072684c
Ryan Jensen <36426759+ryan0jensen@users.noreply.github.com>
2020-05-05 20:00:50 -0400

    Update WTFTest.c

--- WTFTest.c
+++ WTFTest.c
@@ -1,5 +1,24 @@
-//Run TEST cod
-int main(int argc, char** argv) {
-
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <fcntl.h>
+//Run TEST code
+int main(int argc, char** argv){
+	int fd = open(".Commands", RD_ONLY);
+	char* cmd = (char*)malloc(256);
+	char c;
+	int n;
+	int i = 0;
+	while((n = read(fd, &c, 1))>0){
+		if(c=='\n'){
+			cmd[i]='\0';
+			system(cmd);
+			i = 0;
+			bzero(cmd, 255);
+		} else {
+			cmd[i] = c;
+			i++;
+		}
+	}
+	free(cmd);
 	return 0;
 }
